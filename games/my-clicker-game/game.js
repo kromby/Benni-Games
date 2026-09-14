ce5e5cbfc1b5e321d6b18d81fadf5ca66c5215cd
Guðjón Karl <kromby@hress.org>
2026-09-14 20:57:37 +0000

    fix: restore FH-kosmos cps to 1000

--- games/my-clicker-game/game.js
+++ games/my-clicker-game/game.js
@@ -26,7 +26,7 @@
     { id: "kokualdur", name: "FH-aldur", baseCost: 2000, owned: 0, cps: 20 },
     { id: "kokuveldi", name: "FH-veldi", baseCost: 5000, owned: 0, cps: 50 },
     { id: "kokuheimur", name: "FH-heimur", baseCost: 20000, owned: 0, cps: 100 },
-    { id: "kokukosmos", name: "FH-kosmos", baseCost: 100000, owned: 0, cps: 10000 },
+    { id: "kokukosmos", name: "FH-kosmos", baseCost: 100000, owned: 0, cps: 1000 },
     { id: "framleidslutvofoldun", name: "Framleiðslutvöföldun", baseCost: 500000, owned: 0, cpsMultiply: true },
   ];
 
